b79172fbb35979514fd7a08870e9042b49c84c87
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2023-07-21 21:59:26 -0600

    signin with redirect google

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,11 +5,8 @@ import {
 import SigninForm from "../components/onboard/SigninForm"
 import LeftSide from "../components/onboard/LeftSide"
 import logo from './logo.png'; // Tell webpack this JS file uses this image
-import router from "next/router";
-
 
 function Login() {
-    
   return (
     <Grid h='100vh' templateRows='repeat(1, 1fr)' templateColumns='repeat(12, 1fr)' gap={1}>
       <GridItem rowSpan={1} colSpan={6} bg='#01212E' display={{base: 'none', md:'block'}}>
