a2f44193ed7066a0784cba5b802b3331a416fe2d
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2023-07-24 15:41:34 -0600

    landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,21 @@ import { useEffect } from 'react';
 import router from "next/router";
 
 
-function Login() {
-  // useEffect(() => {
-  //     router.push("/chat");
-  //   }, []);
+function Landing() {
+  useEffect(() => {
+      router.push("/login");
+    }, []);
   return (
-    <Grid h='100vh' templateRows='repeat(1, 1fr)' templateColumns='repeat(12, 1fr)' gap={1}>
-      <GridItem rowSpan={1} colSpan={6} bg='#01212E' display={{base: 'none', md:'block'}}>
-          <LeftSide />
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={{base: 12, lg: 6}} bg='white'>
-        <SigninForm />
-      </GridItem>
-    </Grid>
+    <></>
+    // <Grid h='100vh' templateRows='repeat(1, 1fr)' templateColumns='repeat(12, 1fr)' gap={1}>
+    //   <GridItem rowSpan={1} colSpan={6} bg='#01212E' display={{base: 'none', md:'block'}}>
+    //       <LeftSide />
+    //   </GridItem>
+    //   <GridItem rowSpan={1} colSpan={{base: 12, lg: 6}} bg='white'>
+    //     <SigninForm />
+    //   </GridItem>
+    // </Grid>
   );
 }
 
-export default Login
+export default Landing
